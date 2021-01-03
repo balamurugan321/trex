6694bfff0cc906c43ea7b9bc0993d2ad24315b70
balamurugan321 <76898808+balamurugan321@users.noreply.github.com>
2021-01-03 13:59:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -71,7 +71,7 @@ function setup() {
 
   
   trex.setCollider("rectangle",0,0,trex.width,trex.height);
-  trex.debug = true
+  trex.debug = false;
   
   score = 0;
   
@@ -87,7 +87,7 @@ function draw() {
   
   if(gameState === PLAY){
 
-    gameOver.visible = true;
+    gameOver.visible = false;
     restart.visible = false;
     
     ground.velocityX = -(4 + 3* score/100)
